0788f2317b255e91717ccaeb6bc2db5e0e1a0893
i-ankit01 <ankit.common.acc@gmail.com>
2026-02-28 20:38:21 +0530

    update stacks

--- Stacks/NextGreaterElement2.java
+++ Stacks/NextGreaterElement2.java
@@ -1,6 +1,23 @@
 package Stacks;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class NextGreaterElement2 {
+
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for(int i = 2*n-1;  i>= 0; i--){
+            while(!st.isEmpty() && st.peek() <= nums[i%n]) st.pop();
+            if(i<n) ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         
     }
